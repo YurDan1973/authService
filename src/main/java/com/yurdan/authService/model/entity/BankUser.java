980f77b93a49c8f8d0f58c1edc36fc709e7a2837
Jura Danilov <jura.danilov.2024@gmail.com>
2025-05-10 07:48:31 +0300

    Initial commit

--- src/main/java/com/yurdan/authService/model/entity/BankUser.java
+++ src/main/java/com/yurdan/authService/model/entity/BankUser.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BankUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private UUID uuid;
 
